6a3c82af157c7ed928c8b64577164e58b55b90a6
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2023-11-03 12:05:00 -0600

    Make skeleton code for updating customer infoß

--- customer_db_frontend/customer_db.py
+++ customer_db_frontend/customer_db.py
@@ -34,19 +34,60 @@ def print_all_customers():
     except:
       pass
 
+def update_customer_info(field_to_update, new_value, customer_id):
+  # sql_update = "UPDATE Customers SET ?=? WHERE customer_id=?"
+  print(field_to_update)
+  sql_update = "UPDATE Customers SET ?=? WHERE customer_id=?"
+  update_values = (field_to_update, new_value, customer_id)
+  # update_values = (customer_id,)
+  cursor.execute(sql_update, update_values)
+  connection.commit()
+
 def view_customers():
   print_all_customers()
   customer_choice = input("\nEnter a Customer ID to View a Customer\nPress 'Enter' to return to Main Menu\n>>>")
-  was_valid_id = print_customer_details(customer_choice)
-  if was_valid_id:
+  chosen_customer_id = print_customer_details(customer_choice)
+  if chosen_customer_id:
     edit_customer_choice = input("\nTo update a field, enter the first letter of the field. \nTo delete this record, type 'DELETE'\nTo return to the main menu, press 'Enter'.\n>>>")
-  # Determine what to do based on choice, or break out of things
+    # Determine what to do based on choice, or break out of things
+    if edit_customer_choice.lower() == 'i':
+      print('\nThe ID cannot be changed.')
+    elif edit_customer_choice.lower() == 'n':
+      print('\nCurrent Name:')
+      new_name = input('New Name:')
+      # update_customer_info(chosen_customer_id, 'name', new_name)
+    elif edit_customer_choice.lower() == 'a':
+      print('\nCurrent Address:')
+      new_address = input('New Address:')
+    elif edit_customer_choice.lower() == 'c':
+      print('\nCurrent City:')
+      new_city = input('New City:')
+    elif edit_customer_choice.lower() == 's':
+      print('\nCurrent State:')
+      new_state = input('New State:')
+    elif edit_customer_choice.lower() == 'z':
+      print('\nCurrent Zipcode:')
+      new_zipcode = input('New Zipcode:')
+    elif edit_customer_choice.lower() == 'p':
+      print('\nCurrent Phone Number:')
+      new_phone_number = input('New Phone Number:')
+    elif edit_customer_choice.lower() == 'e':
+      print('\nCurrent Email:')
+      new_email = input('New Email:')
+    elif edit_customer_choice.lower() == 'delete':
+      pass
+    # else:
+    #   print('Invalid selection. Please try again.')
 
 def search_customers():
   pass
 
 def add_customer():
   pass
+
+# print('test start')
+# update_customer_info('name', 'TestName123412343215', 34)
+# print('test end')
 
 while True:
   choice = input('\n**** Customer Database ****\n\n[1] View All Customers\n[2] Search Customers\n[3]Add a New Customer\n[Q]Quit\n\n>>>')
